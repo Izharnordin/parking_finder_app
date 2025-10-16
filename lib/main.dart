0a53cacacfe9fe7ca154d2522b34340e4abcc616
Izharnordin <izharnordin03@gmail.com>
2025-10-17 05:58:47 +0800

    Modified to accommodate Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/splash_screen.dart';
+import 'screens/login_page.dart';
 import 'screens/home_page.dart';
 import 'screens/map_page.dart';
-import 'screens/login_page.dart';
-import 'screens/splash_screen.dart';
+import 'screens/settings_page.dart';
+import 'screens/profile_page.dart';
+import 'screens/signup_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyCjhQiN2och7k_c0ByG7KLy5MjA8Tf4Lts", 
+      authDomain: "parking-finder-app-f9f0d.firebaseapp.com",
+      projectId: "parking-finder-app-f9f0d",
+      storageBucket: "parking-finder-app-f9f0d.firebasestorage.app",
+      messagingSenderId: "340914607999",
+      appId: "1:340914607999:web:0a758dd69fd8f1c725ac4a", 
+      measurementId: "G-MNVPYENGQR"
+      )
+  );
   runApp(const ParkingFinderApp());
 }
 
-class ParkingFinderApp extends StatelessWidget {
+class ParkingFinderApp extends StatefulWidget {
   const ParkingFinderApp({super.key});
+
+  @override
+  State<ParkingFinderApp> createState() => _ParkingFinderAppState();
+}
+
+class _ParkingFinderAppState extends State<ParkingFinderApp> {
+  bool _isDarkMode = false;
+
+  void toggleTheme(bool value) {
+    setState(() {
+      _isDarkMode = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Smart Outdoor Parking Finder',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 17, 19, 23)),
-        useMaterial3: true,
-      ),
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
       initialRoute: '/',
       routes: {
-        '/': (context) => const SplashScreen(),  // Default route
+        '/': (context) => const SplashScreen(),
+        '/signup': (context) => const SignUpPage(),
         '/login': (context) => const LoginPage(),
         '/home': (context) => const HomePage(),
         '/map': (context) => const MapPage(),
-        // Future pages:
-        '/list': (context) => const Placeholder(),
-        '/settings': (context) => const Placeholder(),
+        '/settings': (context) => SettingsPage(
+              onThemeChanged: toggleTheme,
+              isDarkMode: _isDarkMode,
+            ),
+        '/profile': (context) => const ProfilePage(),
       },
     );
   }
